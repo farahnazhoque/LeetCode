bf021e5dd00b4ad73bf44faa37973aeb358bde5b
Farahnaz Hoque <107436970+farahnazhoque@users.noreply.github.com>
2024-06-11 19:18:51 -0700

    completed 11.3

---
+++ Day 11/product_of_array_exclude_self.py
@@ -0,0 +1,41 @@
+import math
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # Understanding
+        # questions to ask: 
+        
+        # Matching
+        # Hash map: dictionary of lists
+        # Array
+        
+        # Planning
+        """
+        def productExceptSelf(self, nums: List[int]) -> List[int]:
+            res = []
+
+            for i in range(len(nums)):
+                value = nums.pop(i)
+                res.append(math.prod(nums))
+                nums.insert(i, value)
+            return res
+
+
+        """
+
+
+        # Implementing
+        res = []
+
+        for i in range(len(nums)):
+            value = nums.pop(i)
+            res.append(math.prod(nums))
+            nums.insert(i, value)
+        return res
+
+        # Reviewing: Worked
+
+
+        # Evaluation
+        # Time complexity: O(n)
+        # Space complexity: O(n)
