79466c6b22f6356f91a4945028e47bb0719162b8
Farahnaz Hoque <107436970+farahnazhoque@users.noreply.github.com>
2024-06-16 13:44:52 +0600

    13.2 complete

---
+++ Day 13/two_sum_ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # Understanding
+        # questions to ask: what if the list is empty
+
+        # Matching
+        # use a for loop 
+
+        # Planning
+        """
+        def twoSum(self, numbers: List[int], target: int) -> List[int]:
+            for i in range(len(numbers)):
+                for j in range(i+1, len(numbers)):
+                    if numbers[i] + numbers[j] == target:
+                        return [i, j]
+           
+        """
+
+        # Implementing
+        for i in range(len(numbers)):
+                for j in range(i+1, len(numbers)):
+                    if numbers[i] + numbers[j] == target:
+                        return [i+1, j+1]
+
+        # Review
+        """
+        My initial approach was correct but as I did not
+        observe the example better, I was unable to grasp
+        that I was to return the regular index not the 
+        usual compsci index that starts at 0.
+        """
+
+        # Evaluating
+        # time complexity: O(n^2)
+        # space complexity: O(1)
