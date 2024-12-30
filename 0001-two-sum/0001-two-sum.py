20ce22966471d97f2221699af42309ca619526a8
Farahnaz Hoque <107436970+farahnazhoque@users.noreply.github.com>
2024-12-29 23:16:01 -0800

    Time: 0 ms (100%), Space: 18.8 MB (14.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,9 +2,14 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for n in range(len(nums)):
-            for s in range(n+1, len(nums)):
-                if nums[n] + nums[s] == target:
-                    return [n, s]
-                else:
-                    continue
+        tracking = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num # the value needed to make up the target
+
+            if complement in tracking:
+                return [tracking[complement], i]
+
+            tracking[num] = i # adding to the dictionary if not existing
+
+        return []
