d2b0a1d1d865a66e3fe7672a5f183bf1b3cb7e76
Farahnaz Hoque <107436970+farahnazhoque@users.noreply.github.com>
2024-12-30 00:14:18 -0800

    Time: 1 ms (62%), Space: 18.9 MB (10.25%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -6,7 +6,7 @@ class Solution:
         j = 1
 
         for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
+            if nums[i] != nums[i-1]: # when the elements are different, swap one of the duplicate elements with this new different element and increment j, where the next swap will take place early on
                 nums[j] = nums[i]
                 j += 1
         return j
